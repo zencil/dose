0343e7cd0ebba953b8d87634d91501133945cb13
Jonathan Jimson <orbitron.hd.official@gmail.com>
2026-03-19 15:21:57 +0530

    Update package name

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.app"
+    namespace = "org.orbitronhd.dose"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.app"
+        applicationId = "org.orbitronhd.dose"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- app/android/app/src/main/kotlin/org/orbitronhd/dose/MainActivity.kt
+++ app/android/app/src/main/kotlin/org/orbitronhd/dose/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.app
+package org.orbitronhd.dose
 
 import io.flutter.embedding.android.FlutterActivity
 
