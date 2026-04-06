6d3971a2f9d422c6c6c35e8258590bb2156a2dfe
Jonathan Jimson <orbitron.hd.official@gmail.com>
2026-04-06 07:06:01 +0530

    feature: register alarm components in AndroidManifest and configure MainActivity for lock screen display

--- app/android/app/src/main/kotlin/org/orbitronhd/dose/MainActivity.kt
+++ app/android/app/src/main/kotlin/org/orbitronhd/dose/MainActivity.kt
@@ -1,5 +1,24 @@
 package org.orbitronhd.dose
 
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import org.orbitronhd.dose.alarm.DoseAlarmPlugin
+import org.orbitronhd.dose.alarm.AlarmForegroundService
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        DoseAlarmPlugin.register(flutterEngine, this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+        }
+        super.onCreate(savedInstanceState)
+    }
+}
